81ab639fd866f2c729a4f805b18f7f79c468f382
khaled94 <khaledbaniamer23@gmail.com>
2022-06-22 16:25:53 +0200

    main

--- task22-6-2022/src/App.js
+++ task22-6-2022/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Sort from "./components/sort";
+import Map from './components/map'
+import Filter from "./components/filter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Sort />
+      <Map />
+      <Filter />
     </div>
   );
 }
